df54dfd42d9dd3c2c555d43034dbc64a9af1c9a2
Renaud HAGER <renaud.hager@argos.co.uk>
2017-10-27 11:07:52 +0100

    Change port variable name to avoid collision.

--- webapp/app.py
+++ webapp/app.py
@@ -28,5 +28,5 @@ def return_headers():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('CONTAINER_PORT', 5000))
     app.run(host='0.0.0.0', port=port)
